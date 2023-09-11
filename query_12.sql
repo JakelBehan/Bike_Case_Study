b6424665c92bc2a0f03cdc6399102ec0d6bafc57
JakelBehan <142445649+JakelBehan@users.noreply.github.com>
2023-09-11 12:02:58 -0400

    Delete query_12.sql

--- query_12.sql
+++
@@ -1,7 +0,0 @@
--- This query checks for null values in the member_casual column --
-
-SELECT DISTINCT member_casual
-FROM `bikes-381516.Bikes_2023_03.All_Bikes_no_nulls`
-WHERE member_casual is null
-      
--- There are no null values --
