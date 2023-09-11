8db510312d517c68a51c8aacfc09c632f3f7d83e
JakelBehan <142445649+JakelBehan@users.noreply.github.com>
2023-09-11 12:18:03 -0400

    Delete query_5.sql

--- query_5.sql
+++
@@ -1,8 +0,0 @@
-
--- This query selects the start_lng column and checks for any null values --
-
-select start_lng
-from `bikes-381516.Bikes_2023_03.All_Bikes`
-where start_lng is null
-
---none are found--
