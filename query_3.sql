2ed4b709f802d86e9c88aee550b0cf344642f087
JakelBehan <142445649+JakelBehan@users.noreply.github.com>
2023-09-11 12:17:41 -0400

    Delete query_3.sql

--- query_3.sql
+++
@@ -1,8 +0,0 @@
--- This query checks for duplicate rides that may have been recorded twice in the data --
-
-SELECT ride_id
-FROM `bikes-381516.Bikes_2023_03.All_Bikes`
-GROUP BY ride_id
-HAVING count(ride_id) > 1
-
--- no duplicate rides are found --
