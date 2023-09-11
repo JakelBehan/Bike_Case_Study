f76c260e552e92f8654af7d6d94aa56a2832a230
JakelBehan <142445649+JakelBehan@users.noreply.github.com>
2023-09-11 12:02:01 -0400

    Delete query_10.sql

--- query_10.sql
+++
@@ -1,8 +0,0 @@
-
--- This query prodives a check to see if the new table is clean --
-
-select end_lng, end_lat
-from `bikes-381516.Bikes_2023_03.All_Bikes_no_nulls`
-where end_lng is null and end_lat is null
-
--- No null values are found --
