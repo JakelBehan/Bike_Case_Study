be1fbb08168f2a5241f8d5993ccd030c6cbb4ff5
JakelBehan <142445649+JakelBehan@users.noreply.github.com>
2023-09-11 12:18:19 -0400

    Delete query_7.sql

--- query_7.sql
+++
@@ -1,8 +0,0 @@
-
--- This query selects the end_lng column and checks for any null values --
-
-select end_lng
-from `bikes-381516.Bikes_2023_03.All_Bikes`
-where end_lng is null
-
--- 4544 null values are found --
