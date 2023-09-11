54e933d0cbd83f8d4b818f1c2eda2bce53bf05cf
JakelBehan <142445649+JakelBehan@users.noreply.github.com>
2023-09-11 12:18:39 -0400

    Delete query_9.sql

--- query_9.sql
+++
@@ -1,9 +0,0 @@
-
--- This query creates a table that excludes the rows with null values from the 
--- end_lat and end_lng columns --
-
-CREATE TABLE `bikes-381516.Bikes_2023_03.All_Bikes_no_nulls`
-AS
-SELECT *
-FROM `bikes-381516.Bikes_2023_03.All_Bikes`
-WHERE end_lat IS NOT NULL;
