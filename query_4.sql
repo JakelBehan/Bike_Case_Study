059e804090f173079c218a1dfb269e17870f0fb7
JakelBehan <142445649+JakelBehan@users.noreply.github.com>
2023-09-11 12:17:55 -0400

    Delete query_4.sql

--- query_4.sql
+++
@@ -1,7 +0,0 @@
--- This query selects the start_lat column and checks for any null values --
-
-select start_lat
-from `bikes-381516.Bikes_2023_03.All_Bikes`
-where start_lat is null
-
---none are found--
