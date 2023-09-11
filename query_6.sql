284c12c6d0bd9029a313f079951339ec6ee43bef
JakelBehan <142445649+JakelBehan@users.noreply.github.com>
2023-09-11 12:18:11 -0400

    Delete query_6.sql

--- query_6.sql
+++
@@ -1,8 +0,0 @@
-
--- This query selects the end_lat column and checks for any null values --
-
-select end_lat
-from `bikes-381516.Bikes_2023_03.All_Bikes`
-where end_lat is null
-
--- 4544 null values are found --
